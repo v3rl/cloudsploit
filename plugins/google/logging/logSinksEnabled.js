a557681f41cac4a09a895f0a7759cc9f65463baa
v3rl <vinaya_vengurlekar@yahoo.in>
2024-07-20 13:17:05 +0100

    Update logSinksEnabled.js
    Mapped CIS benchmark 2.2  Ensure That Sinks Are Configured for All Log Entries

--- plugins/google/logging/logSinksEnabled.js
+++ plugins/google/logging/logSinksEnabled.js
@@ -12,7 +12,9 @@ module.exports = {
     recommended_action: 'Ensure a log sink is configured properly with an empty filter and a destination.',
     apis: ['sinks:list', 'buckets:list'],
     realtime_triggers: ['storage.buckets.create', 'storage.buckets.delete', 'logging.ConfigServiceV2.CreateSink' , 'logging.ConfigServiceV2.DeleteSink', 'logging.ConfigServiceV2.UpdateSink'],
-
+    compliance: {
+        cis1: '2.2 Ensure That Sinks Are Configured for All Log Entries'
+    },
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
